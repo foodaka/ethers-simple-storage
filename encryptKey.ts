7a666afc556ed48a0f641656a53d8de48f280c0e
Mark Hinschberger <foodaka@users.noreply.github.com>
2022-06-16 09:53:28 +0100

    deploy to rinkeby

---
+++ encryptKey.ts
@@ -0,0 +1,25 @@
+// for private key management
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+require("dotenv").config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY
+  );
+
+  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+  console.log("encryptedJsonKey", encryptedJsonKey);
+}
+
+main()
+  .then(() => {
+    return process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
